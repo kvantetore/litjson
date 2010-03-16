b8d4660fc6eb8d3a96e42a6aca48e2fc348022ae
Tore <tore@vostopia.com>
2010-03-16 17:29:30 +0100

    Changed parsing of numbers to using the invariant namespace to make it possible to parse floating point numbers in locales with a different decimal symbol than "."

--- src/LitJson/JsonReader.cs
+++ src/LitJson/JsonReader.cs
@@ -13,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Globalization;
 
 
 namespace LitJson
@@ -257,7 +258,7 @@ namespace LitJson
                 number.IndexOf ('E') != -1) {
 
                 double n_double;
-                if (Double.TryParse (number, out n_double)) {
+                if (Double.TryParse (number, NumberStyles.Float, System.Globalization.NumberFormatInfo.InvariantInfo, out n_double)) {
                     token = JsonToken.Double;
                     token_value = n_double;
 
